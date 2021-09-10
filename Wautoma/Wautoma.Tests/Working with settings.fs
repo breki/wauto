c467c294ada91b02fb38b35e76e9995502854334
Igor Brejc <igor.brejc@gmail.com>
2021-09-10 08:53:37 +0200

    Implemented a simple settings perisistence API.

---
+++ Wautoma/Wautoma.Tests/Working with settings.fs
@@ -0,0 +1,35 @@
+﻿module Tests.``Working with settings``
+
+open Wautoma.Settings
+open Xunit
+open Swensen.Unquote
+open System.IO
+
+
+let deleteAnyExistingSettingsFile settingsFilename =
+    if File.Exists settingsFilename then
+        File.Delete settingsFilename
+
+[<Fact>]
+let ``returns empty settings if settings file does not exist`` () =
+    let settingsFilename = "some-settings1.json"
+    deleteAnyExistingSettingsFile settingsFilename
+
+    let settings = loadSettings settingsFilename
+    test <@ settings = Map.empty @>
+
+[<Fact>]
+let ``can store and retrieve settings`` () =
+    let settingsFilename = "some-settings2.json"
+    deleteAnyExistingSettingsFile settingsFilename
+
+    loadSettings settingsFilename
+    |> setSetting "width" 200
+    |> setSetting "height" 100
+    |> saveSettings settingsFilename
+
+    let settings = loadSettings settingsFilename
+    test <@ settings |> getSetting "width" 0L = 200L @>
+    test <@ settings |> getSetting "height" 0L = 100L @>
+    test <@ settings |> getSettingInt "width" 0 = 200 @>
+    test <@ settings |> getSettingInt "height" 0 = 100 @>
---
+++ Wautoma/Wautoma/Settings.fs
@@ -0,0 +1,48 @@
+﻿module Wautoma.Settings
+
+open System.IO
+open System.Text
+open Newtonsoft.Json
+
+type WautomaSettings = Map<string, obj>
+
+
+let getSetting settingName (defaultValue: 'a) (settings: WautomaSettings) : 'a =
+    match settings.TryFind settingName with
+    | Some value -> value :?> 'a
+    | _ -> defaultValue
+
+
+let getSettingInt
+    settingName
+    (defaultValue: int)
+    (settings: WautomaSettings)
+    : int =
+    match settings.TryFind settingName with
+    | Some value -> value :?> int64 |> int
+    | _ -> defaultValue
+
+
+let setSetting settingName value (settings: WautomaSettings) : WautomaSettings =
+    settings.Add(settingName, value)
+
+let saveSettings (filename: string) settings : unit =
+    let json =
+        JsonConvert.SerializeObject(settings, Formatting.Indented)
+
+    use writer =
+        new StreamWriter(filename, append = false, encoding = Encoding.UTF8)
+
+    writer.Write json
+
+let loadSettings filename : WautomaSettings =
+    if File.Exists filename then
+        use reader =
+            new StreamReader(filename, Encoding.UTF8)
+
+        let json = reader.ReadToEnd()
+
+        JsonConvert.DeserializeObject(json, typeof<WautomaSettings>)
+        :?> WautomaSettings
+    else
+        Map.empty
