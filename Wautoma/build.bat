619b4e6b615d38dd067ea71c45cc4ec79ad23f63
Igor Brejc <igor.brejc@gmail.com>
2021-09-15 09:19:40 +0200

    Added the build script.

---
+++ Wautoma/build.bat
@@ -0,0 +1,20 @@
+@ECHO OFF
+
+dotnet clean --configuration Release || GOTO :error
+dotnet build --configuration Release --no-incremental || GOTO :error
+dotnet test --configuration Release || GOTO :error
+rmdir /s /q Wautoma\bin\Release\net48 || GOTO :error
+dotnet publish Wautoma\Wautoma.fsproj --configuration Release || GOTO :error
+
+ECHO.
+ECHO.
+ECHO BUILD SUCCESSFUL
+CALL :Speak "BUILD SUCCESSFUL"
+GOTO :EOF
+
+:error
+ECHO.
+ECHO.
+ECHO BUILD FAILED
+ECHO Failed with error #%errorlevel%.
+EXIT /b %errorlevel%
